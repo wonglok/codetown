fee92a08da26d72184cf5f6f65b499ecdc68f3dd
Lok Lok <yellowhappy831@gmail.com>
2026-02-14 16:10:26 +0800

    save

--- cli.js
+++ cli.js
@@ -2,7 +2,11 @@
 
 import 'tsx'
 import meow from "meow";
-import { runServer } from './main.js';
+import express from "express";
+import ViteExpress from "vite-express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 const cli = meow(
 	`
 Usage
@@ -33,10 +37,54 @@ Examples
 );
 
 
+async function runServer({
+	host,
+	port,
+	mode
+}) {
+	const app = express();
+	const server = createServer(app);
+
+	app.use(express.static('./dist'));
+
+	const io = new Server(server); // Attach Socket.IO to the HTTP server
+
+	app.get("/hello", (_, res) => {
+		res.send("Hello Vite + React + TypeScript!");
+	});
+
+	io.on("connection", (socket) => {
+		console.log("a web client connected", socket.id);
+
+		socket.on("greet", (arg) => {
+			console.log(arg);
+		});
+		socket.emit("greet", { hello: socket.id });
+
+		socket.on("disconnect", (reason) => {
+			console.log(`Socket disconnected: ${reason}`);
+		});
+	});
+
+	console.log(`Server is: http://${host}:${port}`);
+
+	server.listen(port, host, () => { });
+
+	if (mode === "development") {
+		ViteExpress.config({
+			mode: "development",
+		});
+		ViteExpress.bind(app, server, () => { });
+	}
+}
+
+
+
 runServer({
 	port: `${cli.flags.port || 8077}`,
 	host: `${cli.flags.host || '127.0.0.1'}`,
 	mode: process.env.NODE_ENV === 'development' ? "development" : "production",
 });
+
 
 
